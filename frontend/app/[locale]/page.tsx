7a2904a2d9dc923b77a385fffc7283fbcadb2d5a
Norbert Zborecki <norbertzborecki@gmail.com>
2024-09-19 19:44:53 +0200

    feature: backend - initialization

--- frontend/app/[locale]/page.tsx
+++ frontend/app/[locale]/page.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import { Button } from '@nextui-org/button';
+import { useEffect } from 'react';
 
 import {
   Dialog,
@@ -9,24 +12,42 @@ import {
   DialogTrigger
 } from '#nz/shadcn/components/Dialog';
 
-const Home = () => (
-  <main>
-    <Button color="primary">Button</Button>
-    <Dialog>
-      <DialogTrigger>
-        Trigger
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your account
-            and remove your data from our servers.
-          </DialogDescription>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
-  </main>
-);
+const Home = () => {
+  useEffect(() => {
+    const eventSource = new EventSource('http://localhost:3000/maintenance-mode');
+
+    eventSource.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      console.log(data);
+    };
+
+    eventSource.onerror = (err) => {
+      console.error('Błąd SSE:', err);
+      eventSource.close();
+    };
+
+    return () => eventSource.close();
+  }, []);
+
+  return (
+    <main>
+      <Button color="primary">Button</Button>
+      <Dialog>
+        <DialogTrigger>
+          Trigger
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your account
+              and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </main>
+  );
+};
 
 export default Home;
